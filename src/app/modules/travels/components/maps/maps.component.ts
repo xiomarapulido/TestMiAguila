e53435a2957d22129cd3f45236213845bdc974ce
Xiomara Pulido <xiomara.pulido@banistmo.com>
2020-08-24 22:21:07 -0500

    Goolge API

--- src/app/modules/travels/components/maps/maps.component.ts
+++ src/app/modules/travels/components/maps/maps.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsComponent implements OnInit {
 
+  title = 'AGM project';
+  latitude: number;
+  longitude: number;
+  zoom: number;
+
   constructor() { }
 
   ngOnInit() {
-
+    this.setCurrentLocation();
   }
+
+    // Get Current Location Coordinates
+    private setCurrentLocation() {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
+          this.zoom = 15;
+        });
+      }
+    }
 
 }
--- src/app/modules/travels/travels.module.ts
+++ src/app/modules/travels/travels.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapsComponent } from './components/maps/maps.component';
 import { TravelsRoutingModule } from './travels-routing.module';
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [MapsComponent],
   imports: [
-    CommonModule,
-    TravelsRoutingModule
+    TravelsRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDXIW5Xio12dd6GA-izRqKk9QcY2-wCXFo',
+      libraries: ['places']
+    })
   ]
 })
 export class TravelsModule { }
